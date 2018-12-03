83d54ea25643e4c6d803004d121c56d2d1ea8665
chaitanyarajugithub <chaitanyaraju2@gmail.com>
2018-12-03 12:08:43 +0530

    Modified data

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -41,11 +41,11 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "/Users/naveenkhunteta/Downloads/chromedriver");	
+			System.setProperty("webdriver.chrome.driver", "D:\\Chaitanya_Work\\Githubrepo\\Pageobjectmodule\\chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
-		else if(browserName.equals("FF")){
-			System.setProperty("webdriver.gecko.driver", "/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");	
+		else if(browserName.equals("Firefox")){
+			System.setProperty("webdriver.gecko.driver", "D:\\Chaitanya_Work\\Githubrepo\\Pageobjectmodule\\geckodriver.exe");	
 			driver = new FirefoxDriver(); 
 		}
 		
